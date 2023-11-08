4a4f12d1581f566dcd18d4f40a7ea4ff9bea4329
Tinchodomi <tinchodomi@gmail.com>
2023-11-08 12:22:17 -0300

    mod

--- backend/src/routes/carts.routes.js
+++ backend/src/routes/carts.routes.js
@@ -1,35 +1,16 @@
-import { Router } from 'express';
-import cartsController from '../controllers/carts.controller.js';
-import { authorization, passportError } from '../utils/messageErrors.js';
+import { Router } from "express";
+import cartsController from "../controllers/carts.controller.js";
+//import { authorization, passportError } from "../utils/messageErrors.js";
 const routerCart = Router();
 
-routerCart.get('/', cartsController.getCarts);
-routerCart.get('/:cid', cartsController.getCart);
-routerCart.post('/', cartsController.postCart);
-routerCart.post('/:cid/purchase', cartsController.purchaseCart);
-routerCart.put(
-	'/:cid/product/:pid',
-	passportError('jwt'),
-	authorization('user'),
-	cartsController.putProductToCart
-);
-routerCart.put(
-	'/:cid/products/:pid',
-	passportError('jwt'),
-	authorization('user'),
-	cartsController.putQuantity
-);
-routerCart.put(
-	'/:cid',
-	passportError('jwt'),
-	authorization('user'),
-	cartsController.putProductsToCart
-);
-routerCart.delete('/:cid', passportError('jwt'), authorization('user'), cartsController.deleteCart);
-routerCart.delete(
-	'/:cid/products/:pid',
-	passportError('jwt'),
-	authorization('user'),
-	cartsController.deleteProductFromCart
-);
+routerCart.get("/", cartsController.getCarts);
+routerCart.get("/:cid", cartsController.getCart);
+routerCart.post("/", cartsController.postCart);
+routerCart.post("/:cid/purchase", cartsController.purchaseCart);
+routerCart.put("/:cid/product/:pid", cartsController.putProductToCart);
+routerCart.put("/:cid/products/:pid", cartsController.putQuantity);
+routerCart.put( "/:cid",cartsController.putProductsToCart);
+routerCart.delete( "/:cid",cartsController.deleteCart);
+routerCart.delete("/:cid/products/:pid",cartsController.deleteProductFromCart);
+
 export default routerCart;
